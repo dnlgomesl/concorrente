64f9b85b255558a7a9e1971552a6159e3687ab1e
Daniel Gomes de Lima <50140741+dnlgomesl@users.noreply.github.com>
2022-08-18 17:23:10 -0300

    forksleep

---
+++ forksleep/forksleep.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	join := make(chan int)
+	n := 5
+
+	for i := 0; i < n; i++ {
+		rand.Seed(12)
+		go func(i int) {
+			time.Sleep(time.Second * 5)
+			fmt.Println("hi im:  ", i)
+			join <- 1
+		}(i)
+	}
+
+	for i := 0; i < n; i++ {
+			<-join
+	}
+
+	fmt.Println("done")
+}
